7696843d0e88c5bb420333a05b48cc2cdcc32a17
Florian <florian.kunz@gmail.com>
2016-12-12 22:29:22 +0100

    Updated to latest Angular seed template. Fixed table binding. Added basic "Add" button functionality. Fixed "active" class in toolbar.

---
+++ app/participant.ts
@@ -0,0 +1,9 @@
+export class Participant {
+  level: string;
+  email: string;
+
+  constructor(email: string, level:string) {
+    this.email=email;
+    this.level=level;
+  }
+}
--- app/scheduler.component.ts
+++ app/scheduler.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Participant} from './participant';
+
 @Component({
   selector: 'scheduler',
   templateUrl: 'app/scheduler.component.html'
 })
-export class SchedulerComponent {
+export class SchedulerComponent implements OnInit {
+  currentParticipant: Participant = new Participant("", "required");
+  participants: Participant[];
+  levels: String[] = ["required", "important", "optional"];
+  level: String = "important";
+
+  ngOnInit(): void {
+    this.participants = [
+      new Participant("liam.burke@monarch.com", "required"),
+      new Participant("paul.serene@monarch.com", "optional"),
+      new Participant("beth.wilders@monarch.com", "important"),
+      new Participant("jack.joyce@monarch.com", "required")];
+  }
+
+  get diagnostic() { return JSON.stringify(this.participants); }
+
+  onAdd(): void {
+    this.participants.push(this.currentParticipant);
+    this.currentParticipant = new Participant("", "required");
+  }
 }
